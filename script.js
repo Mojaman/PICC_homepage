d26da9ac68c7e7cda8fcbc06daebe850391cf1d4
moja <moja@example.com>
2026-05-18 19:20:48 +0900

    オーバーレイの作成

--- script.js
+++ script.js
@@ -4,6 +4,12 @@ const globalNav = document.querySelector(".global-nav");
 
 const close = document.getElementById("close");
 
+const overlay = document.querySelector(".overlay");
+
+const wrap_main = document.querySelector(".wrap-main");
+
+const slider = document.getElementById("slider-background");
+
 hamburger.addEventListener("click", () => {
   globalNav.classList.toggle("active");
   globalNav.classList.add("loaded");
@@ -12,3 +18,34 @@ hamburger.addEventListener("click", () => {
 close.addEventListener("click", () => {
   globalNav.classList.toggle("active");
 });
+
+function checkOverLay() {
+  if (isOutOfViewport(slider)) {
+    // slider.classList.add("active");
+    console.log("out of viewport");
+    clearOverLay();
+  } else {
+    // slider.classList.remove("active");
+    console.log("in viewport");
+  }
+}
+
+function isOutOfViewport(el) {
+  const rect = el.getBoundingClientRect();
+
+  return (
+    rect.bottom < 50 ||
+    rect.top > window.innerHeight ||
+    rect.right < 0 ||
+    rect.left > window.innerWidth
+  );
+}
+
+function clearOverLay() {
+  overlay.classList.add("inactive");
+  wrap_main.classList.add("active");
+  window.scrollTo(0, 0);
+  window.removeEventListener("scroll", checkOverLay);
+}
+
+window.addEventListener("scroll", checkOverLay);
